f1d7401e3395205392d3f4e2b86b4905af9439b6
Андрей Полищук <dcnfandrey@mail.ru>
2025-09-14 22:09:00 +0300

    HW DONE

--- List/main.cpp
+++ List/main.cpp
@@ -23,22 +23,158 @@ class List
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class List;
+		friend class Iterator;
+		friend List operator+(const List& left, const List& right);
 
 	}*Head, *Tail; //Объекты классов и структур и указатели на эти объекты можно
 	//объявлять непосредственно после описания классов и структур
 	size_t size; //Размер списка. size_t - это typedef на 'unsigned int'
 public:
-	List()
+	class Iterator
 	{
-		Head = Tail = nullptr;
-		size = 0;
+	protected:
+		Element* Temp;
+	public:
+
+		Iterator(Element* Temp = nullptr) : Temp(Temp)
+		{
+			cout << "ITConstructor:\t" << this << endl;
+		}
+		~Iterator()
+		{
+			cout << "ITDestructor:\t" << this << endl;
+		}
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		Iterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		Iterator operator--(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+		bool operator==(const Iterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		int operator*() const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+	Element* get_Head()const
+	{
+		return Head;
+	}
+	Element* get_Tail()const
+	{
+		return Tail;
+	}
+	size_t get_size()const
+	{
+		return size;
+	}
+	Iterator begin() const
+	{
+		return Head;
+	}
+	Iterator end() const
+	{
+		return nullptr;
+	}
+	List(Element* Head = nullptr, Element* Tail = nullptr, int size = 0) : Head(Head), Tail(Tail), size(size)
+	{
 		cout << "LConstructor:\t" << this << endl;
+	}
+	explicit List(int size) :List()
+	{
+		while (size--) push_front(0);
+		cout << "LSizeConstructor:\t" << this << endl;
+	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		for (int const* it = il.begin(); it != il.end(); it++)
+			push_back(*it);
+		cout << "ListitConstructor:\t" << this << endl;
+	}
+	List(const List& other) : List()
+	{
+		//Deep copy (побитовое копирование)
+		*this = other;
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
+	List(List&& other) noexcept : List()
+	{
+		//Shallow copy (поверхностное копирование)
+		*this = std::move(other);
+		cout << "LMoveConstructor:\t" << this << endl;
 	}
 	~List()
 	{
 		//while (Head)pop_front();
 		while (Tail)pop_back();
 		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//      Operators
+	List& operator=(const List& other)
+	{
+		if (this == &other) return *this; //0) Проверить, что This и Other разные объекты
+		while (Head) pop_front();         //1) Очистить данные
+		//2)Deep copy (побитовое копирование)
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_front(Temp->Data);
+		reverse();
+		cout << "FLCopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	List& operator=(List&& other)
+	{
+		if (this == &other) return *this;
+		while (Head) pop_front();
+		this->Head = other.Head;
+		this->Tail = other.Tail;
+		this->size = other.size;
+		other.Head = nullptr;
+		other.Tail = nullptr;
+		other.size = 0;
+		cout << "FLMoveAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	const int& operator[] (int index) const
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++) Temp = Temp->pNext;
+		return Temp->Data;
+	}
+
+	int& operator[] (int index)
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++) Temp = Temp->pNext;
+		return Temp->Data;
 	}
 
 	//				Adding elements:
@@ -150,6 +286,7 @@ public:
 		}
 		size--;
 	}
+
 	void pop_back() //Меняем зеркально pop_front()
 	{
 		if (Head == nullptr && Tail == nullptr)return; // Если список пуст
@@ -164,6 +301,29 @@ public:
 			delete Tail->pNext;
 			Tail->pNext = nullptr;
 		}
+		size--;
+	}
+
+	void erase(int Index)
+	{
+		if (Index < 0) return;
+		if (Index == 0 || size == 0) return pop_front();
+		if (Index >= size) return pop_back();
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++) Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++) Temp = Temp->pPrev;
+		}
+		Element* Erased = Temp;
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		delete Erased;
 		size--;
 	}
 
@@ -189,8 +349,33 @@ public:
 		cout << delimiter << endl;
 
 	}
+	void reverse()
+	{
+		List reverse;
+		while (Head)
+		{
+			reverse.push_front(Head->Data);
+			pop_front();
+		}
+		*this = std::move(reverse);
+		reverse.Head = nullptr;
+	}
+	friend class Element;
+	friend List operator+(const List& left, const List& right);
 
 };
+
+List operator+(const List& left, const List& right)
+{
+	List fusion;
+	for (List::Element* Temp = left.get_Head(); Temp; Temp = Temp->pNext)
+		fusion.push_front(Temp->Data);
+	for (List::Element* Temp = right.get_Head(); Temp; Temp = Temp->pNext)
+		fusion.push_front(Temp->Data);
+	fusion.reverse();
+	return fusion;
+
+}
 
 
 //#define BASE_CHECK
@@ -235,6 +420,8 @@ void main()
 	for (int i : list1) cout << i << tab; cout << endl;
 	for (int i : list2) cout << i << tab; cout << endl;
 	for (int i : list3) cout << i << tab; cout << endl;
+	list3.erase(5);
+	list3.print();
 #endif // HOME_WORK
 
 
