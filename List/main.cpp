1be75a4208ef6e636645d2379861c89f09d112ef
Андрей Полищук <dcnfandrey@mail.ru>
2025-09-15 20:57:47 +0300

    Operator+ CONST trouble

--- List/main.cpp
+++ List/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 
 #define tab "\t"
@@ -28,11 +30,137 @@ class List
 	//объявлять непосредственно после описания классов и структур
 	size_t size; //Размер списка. size_t - это typedef на 'unsigned int'
 public:
+	class Iterator
+	{
+		Element* Temp;
+	public:
+		Iterator(Element * Temp = nullptr):Temp(Temp) {}
+		~Iterator() {}
+
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		Iterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		Iterator operator--(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+
+		//				Comparison operators:
+		bool operator==(const Iterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		int operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+	class ReverseIterator
+	{
+		Element* Temp;
+	public:
+		ReverseIterator(Element* Temp = nullptr) :Temp(Temp) {}
+		~ReverseIterator() {}
+
+		//					Increment/Decrement:
+		ReverseIterator& operator++()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ReverseIterator operator++(int)
+		{
+			ReverseIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+		ReverseIterator& operator--()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ReverseIterator operator--(int)
+		{
+			ReverseIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+
+		//				Comparison operators:
+		bool operator==(const ReverseIterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const ReverseIterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		int operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+	const Iterator begin()const
+	{
+		return Head;
+	}
+	const Iterator end()const
+	{
+		return nullptr;
+	}
+	ReverseIterator rbegin()
+	{
+		return Tail;
+	}
+	ReverseIterator rend()
+	{
+		return nullptr;
+	}
 	List()
 	{
 		Head = Tail = nullptr;
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
+	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		for (int const* it = il.begin(); it != il.end(); ++it)
+		{
+			push_back(*it);
+		}
+		cout << "ILConstructor:/t" << this << endl;
+	}
+	List(const List& other) :List()
+	{
+		*this = other;
+		cout << "LCopyConstructor:\t" << this << endl;
 	}
 	~List()
 	{
@@ -41,7 +169,17 @@ public:
 		cout << "LDestructor:\t" << this << endl;
 	}
 
-	//				Adding elements:
+	//				Operators:
+	List& operator=(const List& other)
+	{
+		if (this == &other) return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext) push_back(Temp->Data);
+		cout << "LCopyAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	//				Adding Elements:
 	void push_front(int Data)
 	{
 		//1) Создаем элемент, в котором будет храниться добавляемое значение:
@@ -192,6 +330,17 @@ public:
 
 };
 
+List operator+(const List& left, const List& right)
+{
+	List fusion = left;
+	for (List::Iterator it = right.begin(); it != right.end(); ++it)
+	{
+		*it *= 100;
+		fusion.push_back(*it);
+	}
+	return fusion;
+}
+
 
 //#define BASE_CHECK
 #define HOME_WORK
@@ -223,7 +372,7 @@ void main()
 	cout << "Введите индекс добавляемого элемента: "; cin >> index;
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	list.insert(value, index);
-	list.print();
+	list.print(); 
 	list.reverse_print();
 #endif // BASE_CHECK
 
@@ -231,10 +380,22 @@ void main()
 #ifdef HOME_WORK
 	List list1 = { 3, 5, 8, 13, 21 };
 	List list2 = { 34, 55, 89 };
+	//list1.print();
+	//list2.print();
 	List list3 = list1 + list2;
 	for (int i : list1) cout << i << tab; cout << endl;
 	for (int i : list2) cout << i << tab; cout << endl;
 	for (int i : list3) cout << i << tab; cout << endl;
+	for (List::Iterator it = list1.begin(); it != list1.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	for (List::ReverseIterator it = list1.rbegin(); it != list1.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
 #endif // HOME_WORK
 
 
